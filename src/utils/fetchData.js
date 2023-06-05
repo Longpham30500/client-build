82020a2a9ceee182f242d5c07bb2f9ef8c19b770
Phạm Long <phamlong@MacBook-Pro-cua-Long.local>
2023-06-05 20:08:22 +0700

    test host

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const host = 'http://localhost:8080'
+const host = 'http://127.0.0.1:8080'
 
 export const getDataAPI = async (url, token) => {
     const res = await axios.get(`${host}/api/${url}`, {
